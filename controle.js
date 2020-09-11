2bee1580fa0722fffc254d7ef016d3ed465d289e
sofiene <sofieneth17@gmail.com>
2020-09-11 11:21:18 +0100

    version 1

---
+++ controle.js
@@ -0,0 +1,25 @@
+/*
+var obj = {
+   firstname: "foulen",
+   lastname: "benfoulen",
+   age: 52,
+};
+
+console.log(obj.age);
+console.log(obj["age"]);
+
+var users = ["mona", "hadil", "yassmine", "amine", "maram"];
+console.log(users[2]);
+users.push("malek");
+users.splice(3, 1);
+console.log(users);
+*/
+
+var users = ["mona", "hadil", "yassmine", "amine", "maram"];
+
+document.write("<ul>");
+for (var user of users) {
+   document.write("<li>", user, "</li>");
+}
+document.write("</ul>");
+
